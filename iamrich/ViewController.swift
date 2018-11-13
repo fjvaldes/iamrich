145aec56fff4635f8b70ecd56546439162e35574
Francisco Valdes Romero <francisco.valdes@me.com>
2018-11-13 15:36:16 -0300

    Initial Commit

---
+++ iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Francisco Valdés Romero on 11/13/18.
+//  Copyright © 2018 Francisco Valdés Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
